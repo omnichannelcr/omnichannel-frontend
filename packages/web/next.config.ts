f73e14db3909055a798c50bda6b285fba5c27acf
Luis Alpizar Morales <lui.lli2451@gmail.com>
2025-09-26 19:50:41 -0600

    Fix: fixing build

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: undefined,
-  },
+  outputFileTracingRoot: undefined,
 };
 
 export default nextConfig;
